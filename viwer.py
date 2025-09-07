3027fe61fc007bb57518982bfb912e1b998908e3
Carlos <caerlosfreefire@gmail.com>
2025-09-06 21:05:58 -0300

    novas rotas criadas de materias

--- viwer.py
+++ viwer.py
@@ -13,6 +13,15 @@ def blog():
 def conteudos():
     return render_template("btn_conteudos.html")
 
-@app.route("/conteudos_1")
+@app.route("/conteudos_1ANO")
 def conteudos_1():
     return render_template("1_ano.html")
+
+@app.route("/portugues_1Ano")
+def portugues_1():
+    return render_template("portugues_1ano.html")
+
+@app.route("/ingles_1Ano")
+def ingles_1():
+    return render_template("ingles_1ano.html")
+
